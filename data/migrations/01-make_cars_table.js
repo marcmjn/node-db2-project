d814112125bedc2d84c3086c8b93365733c77bed
marcmjn <marcmjn@yahoo.com>
2024-05-13 08:05:12 -0700

    Sprint 14 mod2 done

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments()
+    tbl.text('vin', 17).unique().notNullable()
+    tbl.text('make',50).notNullable()
+    tbl.text('model',50).notNullable()
+    tbl.integer('mileage').notNullable()
+    tbl.text('title', 25)
+    tbl.text('transmission', 25)
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+ return knex.schema.dropTableIfExists('cars')
 };
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,41 @@
 // STRETCH
+const cars = [
+    {
+        vin: '11111111111111101',
+        make: 'toyota',
+        model: 'MK4 supra',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '11111111111110002',
+        make: 'nissan',
+        model: 'R34 Skyline GTR',
+        mileage: 215000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '11111111111110003',
+        make: 'honda',
+        model: 's2000',
+        mileage: 250000,
+        title: 'clean',
+        
+    },
+    {
+        vin: '11111111111110004',
+        make: 'mazda',
+        model: 'rx7 FD3S',
+        mileage: 215000,
+        
+    }
+]
+
+exports.seed = function(knex) {
+    return knex('cars').truncate()
+        .then(function() {
+            return knex('cars').insert(cars)
+        })
+}
